a4575c6f61f8a5a221b3e297b1cac8779aee4562
zerock <zk2020308a@gmail.com>
2024-09-24 09:58:29 +0900

    1st

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+
 import './index.css'
+import {RouterProvider} from "react-router-dom";
+import mainRouter from "./router/mainRouter.tsx";
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <RouterProvider router={mainRouter}></RouterProvider>
 )
---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,10 @@
+
+function MainPage() {
+    return (
+        <div>
+            <div className='text-7xl underline'>Main Page</div>
+        </div>
+    );
+}
+
+export default MainPage;
