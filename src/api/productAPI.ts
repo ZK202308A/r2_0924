d6935e439574f410a7fed8c7623e0d21e3b526c3
zerock <zk2020308a@gmail.com>
2024-09-25 16:43:03 +0900

    0925-Product Add2

--- src/api/productAPI.ts
+++ src/api/productAPI.ts
@@ -3,9 +3,15 @@ import axios from "axios";
 
 const host = 'http://localhost:8089/api/products';
 
+const header = {
+    headers: {
+        'Content-Type': 'multipart/form-data',  // 파일 전송 형식 지정
+    }
+}
+
 export const postAdd = async (formData: FormData): Promise<number> => {
 
-    const res = await axios.post(`${host}/products`, formData)
+    const res = await axios.post(`${host}/`, formData, header)
 
     return Number(res.data.result)
 }
--- src/pages/product/ProductAddPage.tsx
+++ src/pages/product/ProductAddPage.tsx
@@ -1,14 +1,17 @@
-import {ChangeEvent, useState} from "react";
+import {ChangeEvent, useRef, useState} from "react";
+import {postAdd} from "../../api/productAPI.ts";
 
 const initialState = {
     pname:'',
     pdesc: '',
-    price: 0,
+    price: '',
 }
 
 function ProductAddPage() {
 
     const [product, setProduct] = useState({...initialState})
+
+    const filesRef = useRef<HTMLInputElement>(null)
 
     const handleChange = (e:ChangeEvent<HTMLInputElement>) => {
         product[e.target.name] = e.target.value
@@ -16,6 +19,24 @@ function ProductAddPage() {
 
     const handleClick = () => {
         console.log(product)
+
+        const files = filesRef.current.files
+
+        const formData = new FormData()
+
+        for(let i = 0; i<files.length; i++) {
+            formData.append("files", files[i])
+        }
+
+        formData.append("pname", product.pname)
+        formData.append("pdesc", product.pdesc)
+        formData.append("price", product.price)
+
+        postAdd(formData).then(data => {
+            console.log(data)
+            filesRef.current.value = null
+        })
+
     }
 
 
@@ -28,7 +49,7 @@ function ProductAddPage() {
                 <input type='text' name='pname' placeholder='Product Name' onChange={(e) =>handleChange(e)} />
                 <input type='text' name='pdesc' placeholder='Product Desc' onChange={(e) =>handleChange(e)}/>
                 <input type='number' name='price' placeholder='1000' onChange={(e) =>handleChange(e)}/>
-                <input type='file' name='files' multiple={true}/>
+                <input type='file' ref={filesRef} name='files' multiple={true}/>
 
                 <button onClick={handleClick}>ADD</button>
 
